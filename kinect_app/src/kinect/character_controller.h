7a5a8490949b9985f069b4e154eb9a3eb0778952
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-03-09 08:58:22 +0000

    modify add mesh

--- kinect_app/src/kinect/character_controller.h
+++ kinect_app/src/kinect/character_controller.h
@@ -3,20 +3,52 @@
 
 
 #include "kinect_device.h"
+#include <string>
 
 namespace Ogre
 {
 class SceneManager;
 class SceneNode;
 class Entity;
+class Quaternion;
+class Vector3;
+class AnimationState;
+class Degree;
 }
 
 class CharacterController
 {
 private:
-	Ogre::SceneManager* mSceneMgr;
-	Ogre::SceneNode *	m_pCharNode[NUI_SKELETON_POSITION_COUNT];
-	Ogre::Entity *		m_pCharEnt[NUI_SKELETON_POSITION_COUNT];
+	enum AnimID
+	{
+		ANIM_IDLE_BASE,
+		ANIM_IDLE_TOP,
+		ANIM_RUN_BASE,
+		ANIM_RUN_TOP,
+		ANIM_HANDS_CLOSED,
+		ANIM_HANDS_RELAXED,
+		ANIM_DRAW_SWORDS,
+		ANIM_SLICE_VERTICAL,
+		ANIM_SLICE_HORIZONTAL,
+		ANIM_DANCE,
+		ANIM_JUMP_START,
+		ANIM_JUMP_LOOP,
+		ANIM_JUMP_END,
+		ANIM_NONE,
+		NUM_ANIMS = ANIM_NONE
+	};
+	Ogre::SceneManager*			mSceneMgr;
+	Ogre::SceneNode *			m_pCharNode[NUI_SKELETON_POSITION_COUNT];
+	Ogre::Entity *				m_pCharEnt[NUI_SKELETON_POSITION_COUNT];
+	Ogre::SceneNode *			m_pBodyNode;
+	Ogre::Entity *				m_pBodyEnt;
+	Ogre::AnimationState*		m_pAnims[NUM_ANIMS];
+	bool						m_bSwordsDrawn;
+	AnimID						m_eBaseAnimID;                   // current base (full- or lower-body) animation
+	AnimID						m_eTopAnimID;                    // current top (upper-body) animation
+	bool						m_bFadingIn[NUM_ANIMS];            // which animations are fading in
+	bool						m_bFadingOut[NUM_ANIMS];           // which animations are fading out
+	
 public:
 	CharacterController(Ogre::SceneManager* mgr);
 	~CharacterController();
@@ -24,6 +56,14 @@ public:
 	void init(void);
 	void update(NUI_SKELETON_FRAME &frame);
 	void release(void);
+	Ogre::Quaternion calcQuaternion(const Ogre::Vector3 &child, const Ogre::Vector3 &parent);
+
+	void setupBone(const std::string& name, const Ogre::Quaternion& q);
+	void setupBone(const std::string& name,const Ogre::Degree& yaw,const Ogre::Degree& pitch,const Ogre::Degree& roll);
+
+	virtual void setupAnimations();
+	virtual void setBaseAnimation(AnimID id, bool reset = false);
+	virtual void setTopAnimation(AnimID id, bool reset = false);
 };
 
 #endif // _CHARACTER_CONTROLLER_H_
