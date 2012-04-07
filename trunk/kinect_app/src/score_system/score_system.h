aafddaf331e2ab213057fa75908151b7675f7f85
kirai.chang@gmail.com <kirai.chang@gmail.com@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-04-07 05:28:17 +0000

    add ScoreSystem

---
+++ trunk/kinect_app/src/score_system/score_system.h
@@ -0,0 +1,20 @@
+#ifndef _SCORE_SYSTEM_H_
+#define _SCORE_SYSTEM_H_
+
+class ScoreBase;
+class ScoreSystem
+{
+private:
+	ScoreSystem();
+	static ScoreSystem *g_pSystem;
+	int m_iScore;
+public:
+	static ScoreSystem *getSystem(void);
+	virtual ~ScoreSystem(void);
+	static ScoreBase *createScoreObject(unsigned int type);
+	static void resetScore(void);
+	static int calcScore(ScoreBase *bodyPart, ScoreBase *attachObj);
+	static int getScore(void);
+};
+
+#endif _SCORE_SYSTEM_H_
