aa6a842fb0a12899710f92be8792c98485679aa2
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-04-30 07:44:58 +0000

    add wave system and stage data

---
+++ trunk/the-mosquito-killer/src/score_system/wave_system.cpp
@@ -0,0 +1,102 @@
+#include "wave_system.h"
+#include "game_system.h"
+
+#include <iostream>
+#include <fstream>
+#include <json_spirit.h>
+
+WaveSystem::WaveSystem(void):
+	m_fTotalTime(0),
+	m_fTimepass(0),
+	m_iCurrentIndex(0)
+{
+}
+
+WaveSystem::~WaveSystem(void)
+{
+}
+
+void WaveSystem::init(unsigned int stage)
+{
+	char fileName[128];
+	sprintf(fileName, "../stage/stage%.3d.dat", stage);
+	std::fstream file(fileName);
+	if(file.is_open())
+	{
+		m_fTotalTime = 0;
+		m_fTimepass = 0;
+		m_iCurrentIndex = 0;
+		m_vMosquitoData.clear();
+
+		json_spirit::mValue value;
+		json_spirit::read(file, value);
+		json_spirit::mArray arr;
+		json_spirit::mObject obj;
+		arr = value.get_obj()["mosquito"].get_array();
+
+		for(int i = 0; i <arr.size();i++)
+		{
+			obj = arr[i].get_obj();
+			MosquitoData data;
+			if(obj["m_eType"].get_str() == "eMosquitoBase")
+				data.m_eType = eMosquitoBase;
+			else if(obj["m_eType"].get_str() == "eMosquitoSplit")
+				data.m_eType = eMosquitoSplit;
+			else
+				data.m_eType = eMosquitoFat;
+
+			data.m_sModelName = obj["m_sModelName"].get_str();
+
+			for(int i = 0; i < 3; i++)
+			{
+				data.m_vfScale[i] = obj["m_vfScale[3]"].get_array()[i].get_real();
+				data.m_vfPos[i] = obj["m_vfPos[3]"].get_array()[i].get_real();
+				data.m_vfQuat[i] = obj["m_vfQuat[4]"].get_array()[i].get_real();
+			}
+			data.m_vfQuat[3] = obj["m_vfQuat[4]"].get_array()[3].get_real();
+			data.m_fMoveSpeed = obj["m_fMoveSpeed"].get_real();
+
+			data.m_iScore = obj["m_iScore"].get_int();
+			data.m_iParaData = obj["m_iParaData"].get_int();
+			data.m_fTimepass = obj["m_fTimepass"].get_real();
+			data.m_iCount = obj["m_iCount"].get_int();
+			m_vMosquitoData.push_back(data);
+			m_fTotalTime += data.m_fTimepass;
+		}
+	}
+	else
+	{
+
+	}
+}
+
+void WaveSystem::release(void)
+{
+	m_vMosquitoData.clear();
+}
+
+bool WaveSystem::work(float timepass)
+{
+	m_fTimepass += timepass;
+	if(m_iCurrentIndex < m_vMosquitoData.size())
+	{
+		if(m_fTimepass > m_vMosquitoData[m_iCurrentIndex].m_fTimepass)
+		{
+			for(int i = 0; i < m_vMosquitoData[m_iCurrentIndex].m_iCount; i++)
+			{
+				GameSystem::getInstance()->createMosquito(m_vMosquitoData[m_iCurrentIndex].m_eType,
+					m_vMosquitoData[m_iCurrentIndex].m_sModelName.c_str(),
+					0.0,
+					m_vMosquitoData[m_iCurrentIndex].m_vfScale,
+					m_vMosquitoData[m_iCurrentIndex].m_vfPos,
+					m_vMosquitoData[m_iCurrentIndex].m_vfQuat,
+					m_vMosquitoData[m_iCurrentIndex].m_iScore,
+					m_vMosquitoData[m_iCurrentIndex].m_iParaData);
+			}
+			m_iCurrentIndex++;
+			m_fTimepass = 0.0;
+		}
+		return false;
+	}
+	return true;
+}
