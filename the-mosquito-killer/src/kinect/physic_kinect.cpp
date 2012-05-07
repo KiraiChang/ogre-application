ccb89c59306459b992a0e035f6446ac51156dd26
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-05-07 05:32:31 +0000

    physic need mass to simulation physic collision

--- the-mosquito-killer/src/kinect/physic_kinect.cpp
+++ the-mosquito-killer/src/kinect/physic_kinect.cpp
@@ -56,7 +56,7 @@ void PhysicKinect::init(DWORD id)
 		scale[2] = obj["left_hand_ridigi_body"].get_obj()["scale"].get_array()[2].get_real();
 		PhysicDebug *debug = new OgrePhysicDebug();
 		((OgrePhysicDebug *)debug)->init(m_pSceneMgr);
-		m_pBody[eKinectLeftHand] = createRidigBody(obj["left_hand_ridigi_body"].get_obj()["mesh_name"].get_str().c_str(), 0.0, scale, pos, quat, debug, ScoreSystem::createScoreObject(SCORE_TYPE_HAND), 8);
+		m_pBody[eKinectLeftHand] = createRidigBody(obj["left_hand_ridigi_body"].get_obj()["mesh_name"].get_str().c_str(), 0.0, scale, pos, quat, debug, ScoreSystem::createScoreObject(SCORE_TYPE_HAND), 2);
 		m_iBodyID[eKinectLeftHand] = NUI_SKELETON_POSITION_HAND_LEFT;
 
 		scale[0] = obj["right_hand_ridigi_body"].get_obj()["scale"].get_array()[0].get_real();
@@ -64,7 +64,7 @@ void PhysicKinect::init(DWORD id)
 		scale[2] = obj["right_hand_ridigi_body"].get_obj()["scale"].get_array()[2].get_real();
 		debug = new OgrePhysicDebug();
 		((OgrePhysicDebug *)debug)->init(m_pSceneMgr);
-		m_pBody[eKinectRightHand] = createRidigBody(obj["right_hand_ridigi_body"].get_obj()["mesh_name"].get_str().c_str(), 0.0, scale, pos, quat, debug, ScoreSystem::createScoreObject(SCORE_TYPE_HAND), 8);
+		m_pBody[eKinectRightHand] = createRidigBody(obj["right_hand_ridigi_body"].get_obj()["mesh_name"].get_str().c_str(), 0.0, scale, pos, quat, debug, ScoreSystem::createScoreObject(SCORE_TYPE_HAND), 2);
 		m_iBodyID[eKinectRightHand] = NUI_SKELETON_POSITION_HAND_RIGHT;
 
 		scale[0] = obj["left_hand"].get_obj()["scale"].get_array()[0].get_real();
