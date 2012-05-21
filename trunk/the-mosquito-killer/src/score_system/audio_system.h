1ccd35346075135ad9409691242f64d0edcb6a57
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-05-21 07:27:03 +0000

    add sound device

--- trunk/the-mosquito-killer/src/score_system/audio_system.h
+++ trunk/the-mosquito-killer/src/score_system/audio_system.h
@@ -17,15 +17,25 @@ class AudioSystem
 private:
 	static AudioSystem *										g_pInstance;
 	irrklang::ISoundEngine*										m_pEngine;
+	float														m_fMasterVolume;
+	float														m_fBGMVolume;
+	float														m_fSoundVolume;
+	bool														m_bIsSetVolume;
 	V_SOUND														m_vSound;
+	irrklang::ISound *											m_pBGM;
 																AudioSystem(void);
 public:
 	static AudioSystem *										getInstance(void);
 	virtual														~AudioSystem(void);
+	virtual void												restart(void);
 	virtual void												release(void);
-	virtual void												play2D(const char *fileName);
+	virtual void												play2D(const char *fileName, bool isLoop = true, bool startPaused = false);
 	virtual void												play3D(const char *fileName, float *vec, float minDist = 5.0f, bool isLoop = false, bool startPaused = false);
+	virtual void												play3DBGM(const char *fileName, float *vec, float minDist = 5.0f, bool isLoop = true, bool startPaused = false);
 	virtual void												setListenerPosition(float *pos, float *dir);
+	virtual void												setMasterVolune(float volume);
+	virtual void												setBGMVolume(float volume);
+	virtual void												setSoundVolume(float volume);
 	virtual void												update(float timePass);
 };
 
