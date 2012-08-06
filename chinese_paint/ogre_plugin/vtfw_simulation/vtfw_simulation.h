ce7f564585d4809f2d11b71cfd00e3fa5c1baf3f
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-08-06 08:16:43 +0000

    using shader to render but need get shader result

---
+++ chinese_paint/ogre_plugin/vtfw_simulation/vtfw_simulation.h
@@ -0,0 +1,43 @@
+#ifndef _VTFW_SIMULATION_H_
+#define _VTFW_SIMULATION_H_
+#include <iostream>
+#include "../simulation_interface/simulation_interface.h"
+namespace Ogre
+{
+	class SceneManager;
+	class SceneNode;
+	class AnimationState;
+	class ParticleSystem;
+	class ParticleEmitter;
+	class Camera;
+	class Entity;
+}
+
+class WaterInterface;
+
+class VTFWSimulation : public SimulationInterface
+{
+private:
+	Ogre::SceneManager*							m_pSceneMgr;
+	Ogre::Camera*								m_pCamera;
+
+	Ogre::SceneNode*							m_pHeadNode ;
+
+	WaterInterface*								m_pWaterInterface;
+	Ogre::Entity*								m_pWaterEntity ;
+
+	float										m_fTimeoutDelay ;
+	float										m_fHeadDepth ;
+	float										m_fMoveSpeed;
+public:
+												VTFWSimulation(Ogre::SceneManager *sceneMgr, Ogre::Camera* cam);
+												~VTFWSimulation(void);
+	void										init(void);
+	void										release(void);
+	void										setupControls(OgreBites::SdkTrayManager* sdkTray);
+	void										sliderMoved(OgreBites::Slider* slider);
+	void										checkBoxToggled(OgreBites::CheckBox* checkBox);
+	void										itemSelected(OgreBites::SelectMenu* menu);
+	void										update(float timePass);
+};
+#endif //_VTFW_SIMULATION_H_
