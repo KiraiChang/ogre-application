733ffcd21055ab8ed0eb1d940ea7bce501f25837
kirai.chang@gmail.com <kirai.chang@gmail.com@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-11-20 15:02:14 +0000

    modify curve draw

--- trunk/chinese_paint/ogre_plugin/stable_fluids/stroke/stroke.h
+++ trunk/chinese_paint/ogre_plugin/stable_fluids/stroke/stroke.h
@@ -9,19 +9,22 @@ namespace Stroke
 	class Stroke
 	{
 	private:
-		//LIST_POINT					m_vControlPoint;
-		LIST_POINT					m_vCPPositive;
-		LIST_POINT					m_vCPNegative;
+		LIST_POINT					m_vControlPoint;
+		//LIST_POINT					m_vCPPositive;
+		//LIST_POINT					m_vCPNegative;
 		Point						m_center;
 		Point						m_preCenter;
 		StrokeDraw *				m_pDrawPositive;
 		StrokeDraw *				m_pDrawNegative;
 		float						m_fExistTime;
+		float						m_fA, m_fB;
 
 	public:
 									Stroke(float time, const V_POINT &vPoint, StrokeDraw *drawNegative, StrokeDraw *drawPositive, const Point &center, const Point &dir);
 									~Stroke(void);
 		void						release(void);
+		void						classifier(const LIST_POINT &vContorlPoint, LIST_POINT &vNegative, LIST_POINT &vPositive);
+
 		void						update(float timePass, float **field, unsigned int size );
 		bool						isExist(void);
 	};
