5b9a9bcd4c7c1b5c8aefc217b4b6038d773c3e58
kirai.chang <kirai.chang@1b508b4f-d8aa-d487-3a0f-d6e944e77bc6>
2012-11-29 06:03:58 +0000

    add moxi material

---
+++ trunk/moxi_imp/resource/Moxi/readme.txt
@@ -0,0 +1,7 @@
+Cg source code for the LBE-based water percolation simulation described in the
+paper:
+
+MoXi: Real-Time Ink Dispersion in Absorbent Paper, ACM Transactions on Graphics
+(SIGGRAPH 2005 issue), Vol. 24, No. 3, August 2005
+
+Last update: 03 May 2005
--- trunk/moxi_imp/src/main/main_sample.h
+++ trunk/moxi_imp/src/main/main_sample.h
@@ -6,31 +6,38 @@
 #include "resource.h"
 #endif
 
+typedef std::map<std::string, Ogre::TexturePtr> M_TEXTURE;
 class MainSample : public BaseApplication, public Ogre::RenderTargetListener
 {
 private:
 	/*****************************************************************
 							User input begin
 	*****************************************************************/
-	Ogre::Light *l1, *l2, *l3;
+	//Ogre::Light *l1, *l2, *l3;
 
-	Ogre::RenderTarget* mMirrorTarget;
-	Ogre::Plane MirrorPlane;
-	Ogre::Entity* mMirrorEnt;
+	//Ogre::RenderTarget* mMirrorTarget;
+	//Ogre::Plane MirrorPlane;
+	//Ogre::Entity* mMirrorEnt;
 
-	Ogre::Entity* mGlowObjEnt;
-	Ogre::SceneNode* mGlowObjNode;
-	Ogre::GpuProgramParametersSharedPtr fpPP;
-	float dtime;
+	//Ogre::Entity* mGlowObjEnt;
+	//Ogre::SceneNode* mGlowObjNode;
+	//Ogre::GpuProgramParametersSharedPtr fpPP;
+	//float dtime;
+	//M_TEXTURE								m_mapTexture;
+	//Ogre::SceneNode*						m_pPaperNode;
+	//Ogre::Entity*							m_pPaper;
 	/*****************************************************************
 							User input end
 	*****************************************************************/
+	//void createTexture();
+	void redirectIOToConsole();
 public:
+	
 	MainSample(void);
 	virtual ~MainSample(void);
 
-	void preRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
-	void postRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
+	//void preRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
+	//void postRenderTargetUpdate(const Ogre::RenderTargetEvent& evt);
 protected:
 	virtual void createScene(void);
 	bool keyPressed( const OIS::KeyEvent &arg );
